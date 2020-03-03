4817ddc1686451eede21045e4adb478eca87d4ce
Adeline <adelinecouture@gmail.com>
2020-03-03 15:42:22 +0100

    ajout modal

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('change');
 });
+
+// const modalBtn = document.querySelector('.modal-btn');
+// const modalBg = document.querySelector('modal-bg');
+// const modalClose = document.querySelector('.modal-close');
+
+// modalBtn.addEventListener('click', function(){
+//     modalBg.classList.add('bg-active')
+// });
+
+// modalClose.addEventListener('click', function(){
+//     modalBg.classList.remove('bg-active');
+// });
